9fdea556c83c45a5f8b73e5190be19f15971116c
9r1nd <9ri1nd@gmail.com>
2024-11-30 01:08:38 +0000

    fix(API - APP): Register location on admin

--- location/admin.py
+++ location/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from location.models import Location
 
-
 # Register your models here.
+admin.site.register(Location)
 
 
 class LocationAdminInline(admin.StackedInline):
